5334e21346b294f23ed4e868cab39ba26bd2df47
pippypal <47214304+pippypal@users.noreply.github.com>
2020-12-23 21:17:09 +0000

    added in highscore activity code and layout
    implmented database on highscore

--- SequenceGame/app/src/main/java/com/example/sequencegame/Scores.java
+++ SequenceGame/app/src/main/java/com/example/sequencegame/Scores.java
@@ -2,13 +2,137 @@ package com.example.sequencegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scores extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
+
+        listView = findViewById(R.id.lv);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        db.emptyHiScores();     // empty table if required
+
+        // Inserting hi scores
+        Log.i("Insert: ", "Inserting ..");
+        db.addHiScore(new HiScore("20 OCT 2020", "Frodo", 12));
+        db.addHiScore(new HiScore("28 OCT 2020", "Dobby", 16));
+        db.addHiScore(new HiScore("20 NOV 2020", "DarthV", 20));
+        db.addHiScore(new HiScore("20 NOV 2020", "Bob", 18));
+        db.addHiScore(new HiScore("22 NOV 2020", "Gemma", 22));
+        db.addHiScore(new HiScore("30 NOV 2020", "Joe", 30));
+        db.addHiScore(new HiScore("01 DEC 2020", "DarthV", 22));
+        db.addHiScore(new HiScore("02 DEC 2020", "Gandalf", 132));
+
+
+        // Reading all scores
+        Log.i("Reading: ", "Reading all scores..");
+        List<HiScore> hiScores = db.getAllHiScores();
+
+
+        for (HiScore hs : hiScores) {
+            String log =
+                    "Id: " + hs.getScore_id() +
+                            ", Date: " + hs.getGame_date() +
+                            " , Player: " + hs.getPlayer_name() +
+                            " , Score: " + hs.getScore();
+
+            // Writing HiScore to log
+            Log.i("Score: ", log);
+        }
+
+        Log.i("divider", "====================");
+
+        HiScore singleScore = db.getHiScore(5);
+        Log.i("High Score 5 is by ", singleScore.getPlayer_name() + " with a score of " +
+                singleScore.getScore());
+
+        Log.i("divider", "====================");
+
+        // Calling SQL statement
+        List<HiScore> top5HiScores = db.getTopFiveScores();
+
+        for (HiScore hs : top5HiScores) {
+            String log =
+                    "Id: " + hs.getScore_id() +
+                            ", Date: " + hs.getGame_date() +
+                            " , Player: " + hs.getPlayer_name() +
+                            " , Score: " + hs.getScore();
+
+            // Writing HiScore to log
+            Log.i("Score: ", log);
+        }
+        Log.i("divider", "====================");
+
+        HiScore hiScore = top5HiScores.get(top5HiScores.size() - 1);
+        // hiScore contains the 5th highest score
+        Log.i("fifth Highest score: ", String.valueOf(hiScore.getScore()) );
+
+        Bundle b = getIntent().getExtras();
+        int userScore = b.getInt("userScore");
+        String username = b.getString("username");
+        if (hiScore.getScore() < userScore) {
+            db.addHiScore(new HiScore("08 DEC 2020", username, userScore));
+        }
+
+        Log.i("divider", "====================");
+
+        // Calling SQL statement
+        top5HiScores = db.getTopFiveScores();
+        List<String> scoresStr;
+        scoresStr = new ArrayList<>();
+
+        int j = 1;
+        for (HiScore hs : top5HiScores) {
+
+            String log =
+                    "Id: " + hs.getScore_id() +
+                            ", Date: " + hs.getGame_date() +
+                            " , Player: " + hs.getPlayer_name() +
+                            " , Score: " + hs.getScore();
+
+            // store score in string array
+            scoresStr.add(j++ + " : "  +
+                    hs.getPlayer_name() + "\t" +
+                    hs.getScore());
+            // Writing HiScore to log
+            Log.i("Score: ", log);
+        }
+
+        Log.i("divider", "====================");
+        Log.i("divider", "Scores in list <>>");
+        for (String ss : scoresStr) {
+            Log.i("Score: ", ss);
+        }
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, scoresStr);
+        listView.setAdapter(itemsAdapter);
+
+    }
+
+    public void Retry(View view)
+    {
+        Intent start = new Intent(this, MainActivity.class);
+        finish();
+        startActivity(start);
+    }
+
+    public void Quit(View view)
+    {
+        finish();
+        System.exit(0);
     }
 }
